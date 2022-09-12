abf45dc84c9dde1492c78a29a9ffa9518e97194d
tom2609 <tomemmerich2609@gmail.com>
2022-09-12 15:04:36 +0200

    Init

---
+++ spiel_full.py
@@ -0,0 +1,60 @@
+import random
+
+HP1 = 50
+HP2 = 50
+gamestatus = True
+
+while True:
+    while gamestatus == True:
+
+        #Spieler Aktion
+        print("\033[36m" "Was willst du tun?")
+        aktion = int(input("1.Angreifen; 2.Heilen" "\033[0m"))
+        if aktion == 1:
+            zahl = random.randrange(1,10)
+            HP2 -= zahl
+            print("Du hast "+ str(zahl) +" Schaden geamcht")
+            if HP2 <= 0:
+                print("Du hast GEWONNEN!")
+                gamestatus = False
+                break
+            else:
+                print("Der Gegner hat noch " + str(HP2) + "HP")
+        elif aktion == 2:
+            zahl = random.randrange(1,7)
+            HP1 += zahl
+            print("Du heilst dich um "+ str(zahl) + "HP")
+            print("Du hast nun "+ str(HP1) +"HP")
+        else:
+            print("Wähle eine zulässig Zahl!")
+            break
+
+        #Gegner Aktion
+        aktion2 = random.randrange(1,3)
+        if aktion2 == 1:
+            zahl = random.randrange(1,10)
+            HP1 -= zahl
+            print("\033[31m" "Der PC hat " + str(zahl) + " Schaden gemacht")
+            if HP1 <= 0:
+                print("Der PC hat gewonnen." "\033[0m")
+                gamestatus = False
+                break
+            else:
+                print("Du hast noch " + str(HP1) + " HP" "\033[0m")
+        else:
+            zahl = random.randrange(1,7)
+            HP2 += zahl
+            print("\033[31m" "Der PC hat sich um " + str(zahl) + "HP geheilt")
+            print("Der PC hat jetzt " + str(HP2) + "HP" "\033[0m")
+
+    if gamestatus == False:
+        restart = int(input("1.Neustarten; 2.Beenden"))
+        if restart == 1:
+            gamestatus = True
+            HP1 = 50
+            HP2 = 50
+            zahl = 0
+        elif restart == 2:
+            break
+        else:
+            print("Wähle zwischen 1 und 2.")
